fbe1068f507cbf36aefed806168d9d0764c3ffb1
HunterMikkelsen <59233814+HunterMikkelsen@users.noreply.github.com>
2025-06-02 16:24:14 -0600

    rename

---
+++ Unrandomizer/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Unrandomizer
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "ChoccyMewks-Unrandomizer";
+        public const string PLUGIN_NAME = "Choccy Mewk's Unrandomizer";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
