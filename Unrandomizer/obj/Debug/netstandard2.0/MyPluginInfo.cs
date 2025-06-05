0dfb12ac62779cca3e0bbf61eb59b8648966fa66
HunterMikkelsen <59233814+HunterMikkelsen@users.noreply.github.com>
2025-06-05 11:17:53 -0600

    remove trash

--- Unrandomizer/obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace HelloWorldPlugin
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "HelloWorldPlugin";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- Unrandomizer/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Unrandomizer
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "ChoccyMewks-Unrandomizer";
-        public const string PLUGIN_NAME = "Choccy Mewk's Unrandomizer";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
